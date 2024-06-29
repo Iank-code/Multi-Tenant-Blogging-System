ba7eb483dff3c687da58cbd2b9d637ef0a7a146f
Iank-code <kian99564@gmail.com>
2024-06-29 20:08:05 +0300

    feat: Adding association

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,4 @@
 class Admin < ApplicationRecord
     belongs_to :organisation
+    has_one_attached :avatar, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_27_123645) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_29_145644) do
+  create_table "admins", force: :cascade do |t|
+    t.integer "organisation_id", null: false
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["organisation_id"], name: "index_admins_on_organisation_id"
+  end
+
   create_table "organisations", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,4 +38,26 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_123645) do
     t.index ["reset_password_token"], name: "index_organisations_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "admins", "organisations"
 end
